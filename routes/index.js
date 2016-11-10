8a411dead737a0674b42f6e380bebf11f2b8b606
Dale Avery <davery@us.ibm.com>
2016-11-10 18:03:41 -0500

    don't hardcode the chaincode id

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var request = require("request");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'PayOnTime'});
+    res.render('index', {title: 'PayOnTime', bag: req.bag});
 });
 
 router.get('/addUser', function (req, res, next) {
@@ -17,16 +17,16 @@ router.post('/addUser', function (req, res, next) {
     for (var key in req.body) {
         args.push('' + req.body[key])
     }
-    
-	console.log("BODY:", req.body);
-	
+
+    console.log("BODY:", req.body);
+
     var body = {
         "jsonrpc": "2.0",
         "method": "invoke",
         "params": {
             "type": 1,
             "chaincodeID": {
-                "name": "c58e3996f6e7034aad8e76793cb3af368db32de4d1a305926693899db2ea48c492440bb220052f39b4b4fbc256956090b8cc0acb3e47a4b17a0d2efe038ac326"
+                "name": process.env['CHAINCODE_ID']
             },
             "ctorMsg": {
                 "function": "init_company",
@@ -42,8 +42,7 @@ router.post('/addUser', function (req, res, next) {
         url: 'http://192.168.99.100:7050/chaincode',
         json: body
     };
-	
-	
+
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
@@ -60,16 +59,16 @@ router.post('/pay', function (req, res, next) {
     for (var key in req.body) {
         args.push('' + req.body[key])
     }
-    
-	console.log("BODY:", req.body);
-	
+
+    console.log("BODY:", req.body);
+
     var body = {
         "jsonrpc": "2.0",
         "method": "invoke",
         "params": {
             "type": 1,
             "chaincodeID": {
-                "name": "c58e3996f6e7034aad8e76793cb3af368db32de4d1a305926693899db2ea48c492440bb220052f39b4b4fbc256956090b8cc0acb3e47a4b17a0d2efe038ac326"
+                "name": process.env['CHAINCODE_ID']
             },
             "ctorMsg": {
                 "function": "newPayment",
@@ -85,8 +84,7 @@ router.post('/pay', function (req, res, next) {
         url: 'http://192.168.99.100:7050/chaincode',
         json: body
     };
-	
-	
+
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
@@ -98,44 +96,48 @@ router.post('/pay', function (req, res, next) {
 });
 
 
-
 router.get('/score/:id', function (req, res, next) {
     res.render('score', {output: req.params.id});
 });
 
 router.post('/score/submit', function (req, res, next) {
 
-var name = req.body.id;
-var id;
+    var name = req.body.id;
+    var id;
 
-console.log(name)
+    console.log(name)
 
-var options = { method: 'POST',
-  url: 'http://192.168.99.100:7050/chaincode',
-  headers: 
-   { 'postman-token': '329f36c3-646f-d21e-ef54-9f0999f11d54',
-     'cache-control': 'no-cache',
-     'content-type': 'application/json' },
-  body: 
-   { jsonrpc: '2.0',
-     method: 'query',
-     params: 
-      { type: 1,
-        chaincodeID: { name: 'c58e3996f6e7034aad8e76793cb3af368db32de4d1a305926693899db2ea48c492440bb220052f39b4b4fbc256956090b8cc0acb3e47a4b17a0d2efe038ac326' },
-        ctorMsg: { function: 'read', args: [String(name)] },
-        secureContext: 'test_user0' },
-     id: 1 },
-  json: true };
+    var options = {
+        method: 'POST',
+        url: 'http://192.168.99.100:7050/chaincode',
+        headers: {
+            'postman-token': '329f36c3-646f-d21e-ef54-9f0999f11d54',
+            'cache-control': 'no-cache',
+            'content-type': 'application/json'
+        },
+        body: {
+            jsonrpc: '2.0',
+            method: 'query',
+            params: {
+                type: 1,
+                chaincodeID: {name: process.env['CHAINCODE_ID']},
+                ctorMsg: {function: 'read', args: [String(name)]},
+                secureContext: 'test_user0'
+            },
+            id: 1
+        },
+        json: true
+    };
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
 
-  console.log(body);
-  
-  id = body.result.message;
-  res.render('index', {userList:id});
-  
-});
+        console.log(body);
+
+        id = body.result.message;
+        res.render('index', {userList: id});
+
+    });
 
 });
 module.exports = router;
