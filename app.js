d52dc116e432b157a10e76a300eaa7b9daedf754
Dale Avery <davery@us.ibm.com>
2016-11-10 18:10:15 -0500

    Enroll and deploy automatically

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,7 +19,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,10 +27,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,24 +38,64 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
 module.exports = app;
+
+// Blockchain setup
+var enrollOptions = {
+    method: 'POST',
+    url: 'http://192.168.99.100:7050/registrar',
+    json: {enrollId: 'test_user0', enrollSecret: 'MS9qrN8hFjlE'}
+};
+
+request(enrollOptions, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log('Enrollment response:', body);
+
+
+    var deployOptions = {
+        method: 'POST',
+        url: 'http://192.168.99.100:7050/chaincode',
+        json: {
+            jsonrpc: '2.0',
+            method: 'deploy',
+            params: {
+                type: 1,
+                chaincodeID: {path: 'https://github.com/PayOnTime/demo/chaincode'},
+                ctorMsg: {function: 'init', args: ['123']},
+                secureContext: 'test_user0'
+            },
+            id: 1
+        }
+    };
+
+    request(deployOptions, function (error, response, body) {
+        if (error) throw new Error(error);
+        process.env['CHAINCODE_ID'] = body.result.message;
+        console.log('Deploy response:', body);
+        // TODO insert spreadsheet parsing code here
+
+    });
+});
+
+
